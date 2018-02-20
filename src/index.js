4ec8515293cda3d9a64720a965c716c7a60551dd
KirylNik <ultramylo@gmail.com>
2018-02-20 22:03:23 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -8,27 +8,32 @@ class Sorter {
 
     add(element) {
         // remove duplicate items
-        let store = {}; // объект для коллекции
-        let itemsLength = element.length || 1; //количество переданных элементов в масииве или 1 если это не массив
-
-        for (let i = 0; i < itemsLength; i++) {
-            let key = element[i] || element; // для каждого элемента создаём свойство
-            store[key] = key;
-        }
-
-        if (this.elements.length == 0) {                    // Если элементов нету вообще,
-            this.elements = this.elements.concat(element);    // то добавить переданное в функции значение сразу.
+        if (typeof (element) == 'string' || 'symbol') {
+            this.elements.push(element);
         } else {
-            for (var i = 0; i < this.elements.length; i++) {
-                if (toString.call(this.elements[i]) == '[object Object]') { // Если переданный элемент объект, то не проверять
-                    break;                                                    // на повторы.
-                }
-                if (store[this.elements[i]]) {   // Если новй переданный элемент уже имеется в массиве, то
-                    delete store[this.elements[i]]   // то удалить переданного.
-                }
+
+            let store = {}; // объект для коллекции
+            let itemsLength = element.length || 1; //количество переданных элементов в масииве или 1 если это не массив
+
+            for (let i = 0; i < itemsLength; i++) {
+                let key = element[i] || element; // для каждого элемента создаём свойство
+                store[key] = key;
             }
-            for (let key in store) {     // Записать в массив все элементы,очищенные выше от повторений.
-                this.elements.push(store[key])
+
+            if (this.elements.length == 0) {                    // Если элементов нету вообще,
+                this.elements = this.elements.concat(element);    // то добавить переданное в функции значение сразу.
+            } else {
+                for (var i = 0; i < this.elements.length; i++) {
+                    if (toString.call(this.elements[i]) == '[object Object]') { // Если переданный элемент объект, то не проверять
+                        break;                                                    // на повторы.
+                    }
+                    if (store[this.elements[i]] && (typeof(store[this.elements[i]]) == typeof(this.elements[i]))) {   // Если новй переданный элемент уже имеется в массиве, то
+                        delete store[this.elements[i]]   // то удалить переданного.
+                    }
+                }
+                for (let key in store) {     // Записать в массив все элементы,очищенные выше от повторений.
+                    this.elements.push(store[key])
+                }
             }
         }
     }
@@ -53,7 +58,11 @@ class Sorter {
         }                                   // и дабавить их во временный массив 'arraySort'
 
         arraySort.sort(this.compare); //отсортировать временный массив
-        indices.sort(this.compare);   //отсортировать индексы
+        indices.sort(compareNumericDefault);   //отсортировать индексы
+
+        function compareNumericDefault(a, b) {  //Сортировка по умолчанию для индексов.
+            return a - b;
+        };
 
         for (var i = 0; i < indices.length; i++) {
             this.elements.splice(indices[i], 1, arraySort[i])
@@ -65,5 +74,4 @@ class Sorter {
         this.compare = compareFunction;
     }
 }
-
 module.exports = Sorter;
